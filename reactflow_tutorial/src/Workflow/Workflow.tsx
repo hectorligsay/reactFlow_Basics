f83dcea6c9aa5b5221dc2647dc45cb61e90e03a0
Hector Ligsay Jr <hligsay@aol.com>
2025-04-26 13:32:57 -0500

    Got it to work with the first node.

--- reactflow_tutorial/src/Workflow/Workflow.tsx
+++ reactflow_tutorial/src/Workflow/Workflow.tsx
@@ -1,11 +1,20 @@
-import ReactFlow from "reactflow";
+import ReactFlow, { Node } from "reactflow";
 import "reactflow/dist/style.css";
 import { Box } from "@chakra-ui/react";
+
+// build nodes:
+const nodes: Node[] = [
+  {
+    id: "1",
+    data: {},
+    position: { x: 0, y: 0 },
+  },
+];
 
 export const Workflow = () => {
   return (
     <Box height={"500px"} width="500px" border="1px solid black">
-      <ReactFlow />
+      <ReactFlow nodes={nodes} />
     </Box>
   );
 };
