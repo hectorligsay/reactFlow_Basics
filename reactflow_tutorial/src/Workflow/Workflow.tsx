a7a2112dd5d1a60fc28803fea6c38a678b042859
Hector Ligsay Jr <hligsay@aol.com>
2025-04-26 13:43:38 -0500

    Learned how to create the positions for nodes, build edges to connect to nodes, allow user to move freely in the interface.

--- reactflow_tutorial/src/Workflow/Workflow.tsx
+++ reactflow_tutorial/src/Workflow/Workflow.tsx
@@ -1,20 +1,41 @@
-import ReactFlow, { Node } from "reactflow";
+import ReactFlow, {
+  Node,
+  Edge,
+  Background,
+  MiniMap,
+  Controls,
+} from "reactflow";
 import "reactflow/dist/style.css";
-import { Box } from "@chakra-ui/react";
+import { Box, position } from "@chakra-ui/react";
 
 // build nodes:
 const nodes: Node[] = [
   {
     id: "1",
-    data: {},
+    data: {
+      label: "Node 1",
+    },
     position: { x: 0, y: 0 },
   },
+  {
+    id: "2",
+    data: {
+      label: "Node 2",
+    },
+    position: { x: 200, y: 200 },
+  },
 ];
+
+// Build edges
+const edges: Edge[] = [{ id: "1-2", source: "1", target: "2", animated: true }];
 
 export const Workflow = () => {
   return (
     <Box height={"500px"} width="500px" border="1px solid black">
-      <ReactFlow nodes={nodes} />
+      <ReactFlow nodes={nodes} edges={edges} fitView>
+        <Controls />
+        <Background />
+      </ReactFlow>
     </Box>
   );
 };
